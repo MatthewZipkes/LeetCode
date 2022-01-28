0222450076447266613b7c22837243fa8d296de4
Matthew Zipkes <matthewzipkes@gmail.com>
2022-01-28 08:44:25 -0600

    Time: 80 ms (87.74%), Space: 45.2 MB (5.39%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+  let slowPointer = head;
+  let fastPointer = head;
+  if (head && head.next && head.next.next) {
+    while (fastPointer.next !== null && fastPointer.next.next !== null) {
+      if (slowPointer.next === fastPointer.next.next) {
+        return true
+      }
+      slowPointer = slowPointer.next;
+      fastPointer = fastPointer.next.next;
+    } 
+  }
+  return false
+};
