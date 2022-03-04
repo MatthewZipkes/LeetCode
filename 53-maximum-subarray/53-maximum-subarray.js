632b9b985ad18ad8ad96305ae38bf03e6cff2588
Matthew Zipkes <matthewzipkes@gmail.com>
2022-03-04 08:58:41 -0600

    Time: 100 ms (73.18%), Space: 50.5 MB (33.83%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -3,14 +3,11 @@
  * @return {number}
  */
 const maxSubArray = nums => {
-    let max = nums[0]; // Need a starting number we don't care if it is the actual max
-    let current = Math.max(max, 0); //get the biggest number out of these two
-    
-    for (let i = 1; i < nums.length; i += 1) { //loop through nums (we start at 1 because we have the first element)
-        current += nums[i]; //add next value in array
-        max = Math.max(max, current); // if current is greater than max then max = current
-        current = Math.max(current, 0); // if current is not greater than 0 current will be 0 
-    }
-    
-    return max;
+  let maxSum = nums[0]
+  let currMax = maxSum;
+  for (let i = 1; i < nums.length; i++) {
+    currMax = Math.max(nums[i], currMax + nums[i])
+    maxSum = Math.max(currMax, maxSum)
+  }
+  return maxSum
 };
