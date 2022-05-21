b75ae72a628714e5c55a6d40f8e7a986cc3410e6
Matthew Zipkes <matthewzipkes@gmail.com>
2022-05-20 19:15:44 -0500

    Time: 73 ms (67.15%), Space: 42.1 MB (78.27%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -4,21 +4,29 @@
  */
 
 var isValid = function(s) {
+  // check to see if the string is balanced if not return false
+  // create a stack 
+  // create an object that stores the pairs of parenthases
+  // loop through the string
+    // create a char variable 
+    //if we see an open paren add the closing to stack
+    // else if the pop from the stack and see if it equals the char if it doesn't return false
+  //stack sould be empty by the end of the algo if it is return true it is valid else return false
   if (s.length % 2 !== 0) {
     return false
-  }
-
+  } 
+  
   const stack = [];
-  const map = {
+  const parens = {
     '(' : ')',
     '[' : ']',
     '{' : '}'
   }
+  
   for (let i = 0; i < s.length; i++) {
-    let char = s[i];
-    if (map[char]) {
-      stack.push(map[char])
-      // console.log(stack.pop())
+    let char = s[i]
+    if (parens[char]) {
+      stack.push(parens[char])
     } else if (char !== stack.pop()) {
       return false
     }
